11674906b73c99c7e50ed1a154a536f32f8adfa1
Adrian PK <adrianpk@adrianpk.com>
2018-04-20 15:06:16 -0300

    Project dependencies update.

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject pulap-client "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [reagent "0.7.0"]
                  [re-frame "0.10.5"]
                  [secretary "1.2.3"]
                  [garden "1.3.2"]
-                 [ns-tracker "0.3.0"]]
+                 [ns-tracker "0.3.0"]
+                 [cljs-ajax "0.7.3"]
+                 [day8.re-frame/http-fx "0.1.6"]
+                 [com.pupeno/free-form "0.6.0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-garden "0.2.8"]
@@ -67,8 +71,9 @@
                     :asset-path           "js/compiled/out"
                     :source-map-timestamp true
                     :preloads             [devtools.preload
-                                           day8.re-frame-10x.preload
-                                           re-frisk.preload]
+                                           ;; day8.re-frame-10x.preload
+                                           ;; re-frisk.preload
+                                           ]
                     :closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true
                                            "day8.re_frame.tracing.trace_enabled_QMARK_" true}
                     :external-config      {:devtools/config {:features-to-install :all}}
